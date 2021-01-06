a7b13cecd33bfffc0e2991e1aed09640bc6f3b0c
Akito Kasai <kasai@akito19.com>
2021-01-07 01:12:40 +0900

    Add *, / and nested expression

--- mcc.c
+++ mcc.c
@@ -20,8 +20,26 @@ struct Token {
     char *str;      // Token string
 };
 
-Token *token; // Token that the process is focusing.
+// Type of AST nodes
+typedef enum {
+    ND_ADD, // `+`
+    ND_SUB, // `-`
+    ND_MUL, // `*`
+    ND_DIV, // `/`
+    ND_NUM, // Int
+} NodeKind;
 
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind; // Type of Node
+    Node *lhs;     // Left-Hand side
+    Node *rhs;     // Right-Hand side
+    int val;       // Use this value if `kind` is ND_NUM.
+};
+
+// Global variables
+Token *token; // Token that the process is focusing.
 char *user_input;
 
 // Function for error to report the location of error occurrence.
@@ -94,13 +112,10 @@ Token *tokenize() {
             p++;
             continue;
         }
-
-
-        if (*p == '+' || *p == '-') {
+        if (strchr("+-*/()", *p)) {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
-
         if (isdigit(*p)) {
             cur = new_token(TK_NUM, cur, p);
             cur->val = strtol(p, &p, 10);
@@ -114,6 +129,97 @@ Token *tokenize() {
     return head.next;
 }
 
+//
+// Generate AST nodes
+//
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs  = lhs;
+    node->rhs  = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val  = val;
+    return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *primary() {
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_node_num(expect_number());
+}
+
+// Generate assembly code
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("    push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("    pop rdi\n");
+    printf("    pop rax\n");
+
+    switch (node->kind) {
+        case ND_ADD:
+            printf("    add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("    sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("    imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("    cqo\n");
+            printf("    idiv rdi\n"); // idiv: signed division
+            break;
+    }
+
+    printf("    push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "The number of arguments is invalid\n");
@@ -122,24 +228,17 @@ int main(int argc, char **argv) {
 
     user_input = argv[1];
     token = tokenize();
+    Node *node = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
 
-    // The initial of expression must be Integer.
-    printf("    mov rax, %d\n", expect_number());
+    // Generate assembly code by descending AST nodes
+    gen(node);
 
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("    add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf(" sub rax, %d\n", expect_number());
-    }
-
+    // Load stack-top value into RAX register and return.
+    printf("    pop rax\n");
     printf("    ret\n");
     return 0;
 }
